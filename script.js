0ca44ff5bee58815b182100cb9986eb1417051f6
Enerel Tsogt-Ochir <enerel.tsogtochir@gmail.com>
2018-02-22 21:17:15 -0500

    initial adding of files to my repository

---
+++ script.js
@@ -0,0 +1,22 @@
+(function($){
+
+   $(window).on('load', function() { 
+      	var element = document.getElementById('top-heading');
+		element.style.styleFloat = 'right';
+		element.style.cssFloat = 'right';
+    });
+
+    // and/or
+
+    $(document).ready(function(){
+        // the dom is in place, but everything is not necessarily loaded
+    });
+
+})(jQuery);
+
+
+
+
+	
+
+
