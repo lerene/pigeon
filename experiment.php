5c04a405644a309cdb27cf83acd56d754deb2509
Enerel Tsogt-Ochir <enerel.tsogtochir@gmail.com>
2018-02-28 19:51:23 -0500

    line break

--- experiment.php
+++ experiment.php
@@ -1,5 +1,10 @@
 <?php
+
 echo "Welcome to Washington Digital Media \n";
+
 date_default_timezone_set('America/New_York');
+
+echo "\n New line. \n";
 echo date('Y-m-d H:i:s', $_SERVER['REQUEST_TIME']);
+
 ?>
