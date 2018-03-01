23d87cfc9f0ec31b2e058e36b0e40d4f36078213
Enerel Tsogt-Ochir <enerel.tsogtochir@gmail.com>
2018-02-28 19:53:09 -0500

    updating line feed

--- experiment.php
+++ experiment.php
@@ -4,7 +4,7 @@ echo "Welcome to Washington Digital Media \n";
 
 date_default_timezone_set('America/New_York');
 
-echo "\n New line. \n";
+echo "\n\r New line. \n\r";
 echo date('Y-m-d H:i:s', $_SERVER['REQUEST_TIME']);
 
 ?>
