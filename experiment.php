5a509d2fb7e9c8fefc0f4c25fb7aa193c46fedb9
Enerel Tsogt-Ochir <enerel.tsogtochir@gmail.com>
2018-02-28 19:16:11 -0500

    updating time

--- experiment.php
+++ experiment.php
@@ -1,6 +1,4 @@
 <?php
 echo "Welcome to Washington Digital Media \n";
-$date = '2013-09-30 01:16:06';
-$convertDate = date('F jS, Y h:i:s', strtotime($date));
-echo $convertDate;
+echo date('Y-m-d H:i:s', $_SERVER['REQUEST_TIME']);
 ?>
