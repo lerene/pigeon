847560494064f6979d755e8dac1d69810396826a
Enerel Tsogt-Ochir <enerel.tsogtochir@gmail.com>
2018-03-02 21:14:17 -0500

    adding insert statement

--- process.php
+++ process.php
@@ -1,4 +1,8 @@
 <?php
+
+include('dbConfig.php');
+
+
 if (isset($_POST['restaurant_name'])) {
     echo $_POST['restaurant_name'];
 } else {
@@ -9,3 +13,8 @@ if (isset($_POST['restaurant_address'])) {
 } else {
     // Fallback behaviour goes here
 }
+
+ $sql = "INSERT INTO restaurant (restaurant_name, restaurant_address)
+    VALUES ('".$_POST["restaurant_name"]."','".$_POST["restaurant_address"]."')";
+
+    $result = mysqli_query($conn,$sql);
