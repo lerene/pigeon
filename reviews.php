6e3052e1865b0f095812f837c1a584b5ac417bbe
Enerel Tsogt-Ochir <enerel.tsogtochir@gmail.com>
2018-03-03 10:11:45 -0500

    added contact number in reviews.php

--- reviews.php
+++ reviews.php
@@ -5,6 +5,15 @@
          Restaurant name: <input type = "text" name = "restaurant_name" />
          <br>
          Restaurant address: <input type = "text" name = "restaurant_address" /> 
+         Restaurant contact number: <input type="number" name="restaurant_phone">
          <input type="submit" name="submit" value="Add Restaurant" />
 
       </form>
+
+      <!-- pseudo code -->
+
+      <!-- first get rows of data from table restaurant -->
+
+      <!-- then start a php loop -->
+
+      <!-- inside the loop, show all the rows using print or echo -->
