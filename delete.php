09d19b97ae42b51513680df8f423a213c560d269
Enerel Tsogt-Ochir <enerel.tsogtochir@gmail.com>
2018-03-07 21:10:37 -0500

    testing variable mechanics

--- delete.php
+++ delete.php
@@ -1,17 +1,22 @@
 <?php
 
-
+$rabbit = 777;
 if (isset($_POST['restaurant_id'])) {
    echo 'attempting to delete row ';
+    echo $rabbit .'<br />';
+
     echo $_POST['restaurant_id'];
 } else {
     // Fallback behaviour goes here
 }
 
+$rest_id=restaurant_id;
+
+
 $link = mysqli_connect("localhost", "root", "", "soup");
 
 
- $sql = "DELETE FROM restaurant WHERE restaurant_id = 2";
+ $sql = "DELETE FROM restaurant WHERE restaurant_id = '".$rest_id."'";
 
     $result = mysqli_query($link, $sql);
 
