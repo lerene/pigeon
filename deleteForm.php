7c176ba89f264698c59b0aeb66fab69b997202b5
Enerel Tsogt-Ochir <enerel.tsogtochir@gmail.com>
2018-03-12 22:18:16 -0400

    deleted restaurant id words

--- deleteForm.php
+++ deleteForm.php
@@ -3,9 +3,9 @@
  
  	<form action="delete.php" method="POST">
         
- 		--- <br>
+ 		
 
-         Restaurant id: <input type = "number" name = "restaurant_id" value="<?php echo $mcdonalds; ?>"/> <input type = "number" name = "street_num" value="100" />
+         <input type = "number" name = "restaurant_id" value="<?php echo $mcdonalds; ?>"/> 
          <br>
        
          <input type="submit" name="submit" value="Delete Restaurant" />
