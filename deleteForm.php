81ab0491c11d4e159b3fdd72b2e4bd7086b7523c
Enerel Tsogt-Ochir <enerel.tsogtochir@gmail.com>
2018-03-12 22:13:41 -0400

    testing new input field

--- deleteForm.php
+++ deleteForm.php
@@ -5,7 +5,7 @@
         
  		--- <br>
 
-         Restaurant id: <input type = "number" name = "restaurant_id" value="<?php echo $mcdonalds; ?>"/>
+         Restaurant id: <input type = "number" name = "restaurant_id" value="<?php echo $mcdonalds; ?>"/> <input type = "number" name = "street_num" value="100" />
          <br>
        
          <input type="submit" name="submit" value="Delete Restaurant" />
